054c96da4f1e2c7660a9669b8629939d2bc405c3
Ashis Swain <ashisswainofficial77@gmail.com>
2026-01-11 13:59:47 +0530

    backend initialized and Database Connected.

---
+++ backend/config/db.js
@@ -0,0 +1,6 @@
+import mongoose from 'mongoose';
+
+export const ConnectDB = async () => {
+    await mongoose.connect('mongodb+srv://ashisswainofficial77_db_user:quizapp12@cluster0.129gul9.mongodb.net/QuizApp')
+        .then(() => {console.log('DB Connected')});
+}
---
+++ backend/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config.js'
+import {ConnectDB} from "./config/db.js";
+
+const app = express();
+const port = 4000;
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+// DB
+ConnectDB();
+
+// ROUTES
+app.get('/', (req, res) => {
+    res.send('API Working');
+});
+
+app.listen(port, () => console.log(`Server running on http://localhost:${port}`));
